7ed409ea88f1a9225319d083b1d213cd3011e7a7
rauliesan <raullsdev@gmail.com>
2025-05-20 22:59:33 +0200

    Edición general de estilos y del index.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
-function iniciarSesion(){
+function iniciarSesion() {
     window.location.href = "login_es.html";
+}
+
+
+// JavaScript para los FAQ
+function toggleFAQ(element) {
+    const answer = element.nextElementSibling;
+    const icon = element.querySelector('i');
+
+    if (answer.classList.contains('show')) {
+        answer.classList.remove('show');
+        icon.className = 'fas fa-plus';
+    } else {
+        answer.classList.add('show');
+        icon.className = 'fas fa-minus';
+    }
 }
